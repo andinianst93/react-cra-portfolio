7594b23caf2fcdb16f6499bb6a170620a35a1b25
andinianst93 <andinianst93@gmail.com>
2022-09-27 06:24:23 +0700

    sitemap

---
+++ sitemap-routes.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './src/components'
+import { Home, About, Contact, Experience, Error } from './src/pages'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/experience' element={<Experience />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='*' element={<Error />}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
